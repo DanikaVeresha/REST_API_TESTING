98869941bef96dd0661d63d3afe165697c7a1f6c
daria <deshdesh288@gmail.com>
2024-07-18 19:53:52 +0300

    2

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='REST_API_testing',
+    version='0.0.1',
+    packages=['REST_API_testing'],
+    install_requires=[
+        'requests',
+        'pytest',
+    ],
+)
+
