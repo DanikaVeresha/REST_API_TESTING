ff5a085419fc5dd4c8b2a2efd888da270e03146e
DanikaVeresha <163162916+DanikaVeresha@users.noreply.github.com>
2024-07-19 18:33:27 +0300

    Update api_test_create_publication_for_user.py

--- REST_API_testing/test/api_test_create_publication_for_user.py
+++ REST_API_testing/test/api_test_create_publication_for_user.py
@@ -10,6 +10,14 @@ import requests
 
 def test_status_code_for_requests_create_publication(
         main_url, headers, user_data, publication_data, user):
+    """
+    1. Create user for test
+    2. Get user from the response
+    3. Get iD new user
+    4. Create publication for this user by user ID
+    Check status code for requests create publication
+    5. After testing delete user
+    """
     assert user is not None
     create_user_publication = requests.post(
         f'{main_url}/{user}/posts', headers=headers, json=publication_data)
@@ -18,6 +26,15 @@ def test_status_code_for_requests_create_publication(
 
 def test_status_code_for_requests_get_publication(
         main_url, headers, user_data, publication_data, user):
+    """
+    1. Create user for test
+    2. Get user from the response
+    3. Get iD new user
+    4. Create publication for this user by user ID
+    5. Get publication for this user by user ID
+    Check status code for requests get publication
+    6. After testing delete user
+    """
     assert user is not None
     requests.post(f'{main_url}/{user}/posts', headers=headers, json=publication_data)
     get_user_publication = requests.get(f'{main_url}/{user}/posts', headers=headers)
@@ -26,6 +43,16 @@ def test_status_code_for_requests_get_publication(
 
 def test_publication_data(
         main_url, headers, user_data, publication_data, user):
+    """
+    1. Create user for test
+    2. Get user from the response
+    3. Get iD new user
+    4. Create publication for this user by user ID
+    5. Get publication for this user by user ID
+    Check TITLE on correctness for publication
+    Check BODY on correctness for publication
+    6. After testing delete user
+    """
     assert user is not None
     requests.post(f'{main_url}/{user}/posts', headers=headers, json=publication_data)
     get_user_publication = requests.get(f'{main_url}/{user}/posts', headers=headers)
@@ -35,6 +62,15 @@ def test_publication_data(
 
 def test_status_code_for_requests_get_publication_after_user_delete(
         main_url, headers, user_data, publication_data, user_id):
+    """
+    1. Create user for test
+    2. Get user from the response
+    3. Get iD new user
+    4. Create publication for this user by user ID
+    5. Delete user
+    5. Get publication for this user by user ID which was deleted
+    Check status code for requests get publication after user delete
+    """
     assert user_id is not None
     requests.post(f'{main_url}/{user_id}/posts', headers=headers, json=publication_data)
     requests.delete(f'{main_url}/{user_id}', headers=headers)
@@ -44,10 +80,23 @@ def test_status_code_for_requests_get_publication_after_user_delete(
 
 def test_publication_data_for_requests_get_publication_after_user_delete(
         main_url, headers, user_data, publication_data, user_id):
+    """
+    1. Create user for test
+    2. Get user from the response
+    3. Get iD new user
+    4. Create publication for this user by user ID
+    5. Delete user
+    5. Get publication for this user by user ID which was deleted
+    Check data of publication for requests get publication after user delete
+    """
     requests.post(f'{main_url}/{user_id}/posts', headers=headers, json=publication_data)
     requests.delete(f'{main_url}/{user_id}', headers=headers)
     get_user_publication = requests.get(f'{main_url}/{user_id}/posts', headers=headers)
     assert get_user_publication.json() == []
+
+
+
+
 
 
 
